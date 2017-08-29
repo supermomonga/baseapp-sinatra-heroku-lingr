dd7fab18fefa7c4144dd01a101d297e8b813ccea
supermomonga <hi@supermomonga.com>
2017-08-29 22:53:06 +0900

    Modular style

--- app.rb
+++ app.rb
@@ -3,36 +3,46 @@
 require 'bundler'
 Bundler.require
 
-require 'sinatra/reloader' if development?
+require 'sinatra/base'
+require 'sinatra/reloader'
 
 LingrBot.configure do |config|
   config.id     = ENV['BOT_ID']
   config.secret = ENV['BOT_SECRET']
 end
 
-post '/' do
-  Thread.new do
-    begin
-      JSON.parse(request.body.read)['events'].map{ |e|
-        e['message']
-      }.each do |m|
-        text = m['text']
-        room_id = m['room']
-        response =
-          case text
-          when /ping/ then 'pong'
-          else nil
-          end
-        LingrBot.say(room_id, response) if response
-      end
-    rescue e
-    end
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
   end
-  content_type :text
-  ''
-end
 
-get '/' do
-  content_type :text
-  'Still Alive'
+  post '/' do
+    Thread.new do
+      begin
+        JSON.parse(request.body.read)['events'].map{ |e|
+          e['message']
+        }.each do |m|
+          text = m['text']
+          room_id = m['room']
+          response =
+            case text
+            when /ping/ then 'pong'
+            else nil
+            end
+          LingrBot.say(room_id, response) if response
+        end
+      rescue e
+      end
+    end
+    content_type :text
+    ''
+  end
+
+  get '/' do
+    content_type :text
+    'Still Alive'
+  end
+
+  run! if app_file == $0
+
 end
--- config.ru
+++ config.ru
@@ -1,6 +1,3 @@
-require 'bundler'
-Bundler.require
-
 require './app'
-run Sinatra::Application
+run App
 
